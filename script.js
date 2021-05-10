11467e2366f9d518e7d1f92e9b62bed4d7de0675
CoderTeam2 <72189661+CoderTeam2@users.noreply.github.com>
2021-05-10 16:38:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var firstName = "AMAN";
+var lastName = "GUPTA";
+var fullName = firstName + " ".concat(lastName);
+console.log(fullName);
+function cal(){
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    console.log(num1 + num2);
+    console.log(num1 - num2);
+    console.log(num1 * num2);
+    console.log(num1 / num2);
+}
